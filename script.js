c7b8384abc5c2eb0ef8f2d147190cd759b5b9faa
Kedia <niskedia@deloitte.com>
2023-03-16 17:39:03 +0530

    Nisha committed

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ console.log('Hello World!');
 console.log('Another try for git message');
 console.log('Watch Vinland Saga');
 
+
+
+
+
 // git commands to push code -
 //git add .
 //git commit -m "some commit message"
